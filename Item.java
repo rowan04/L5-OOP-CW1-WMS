945e8de48ed3215e079f37d0f75fc7c3e4865353
rowan04 <rowan.moss@stfc.ac.uk>
2025-05-02 18:27:04 +0100

    Add basic classes and attributes

---
+++ Item.java
@@ -0,0 +1,7 @@
+public class Item {
+    public int item_id;
+    public int stock;
+    public Supplier supplier;
+    public float price;
+    public String material;
+}
---
+++ Supplier.java
@@ -0,0 +1,12 @@
+public class Supplier {
+    public String name;
+    public int supplier_id;
+    public String description;
+    public String email;
+    public String phone_number;
+    public String postcode;
+    public String address;
+    public int total_orders;
+    public String last_order_date;
+    public static Material[] material;
+}
