e182a9e9d19d91e95a630ef3100b930183edbe19
rowan04 <rowan.moss@stfc.ac.uk>
2025-05-05 17:51:18 +0100

    Create constructors and get methods for suppliers and customers

--- Customer.java
+++ Customer.java
@@ -1,6 +1,69 @@
+import java.util.ArrayList;
+
 public class Customer {
-    public int customer_id;
+    public int customer_id = 3;
+    public String name;
     public String email;
     public String phone_number;
-    public int total_orders;
+    public int total_orders = 0;
+    public String last_order_date = "None";
+    public ArrayList<Customer> Customers;
+
+    // constructor
+    public Customer(
+            int customer_id,
+            String name,
+            String email,
+            String phone_number
+    ) {
+        this.customer_id = getNewID();
+        this.name = name;
+        this.email = email;
+        this.phone_number = phone_number;
+    }
+
+    // returns customer name
+    public String getName() {
+        return name;
+    }
+
+    // returns customer email
+    public String getEmail() {
+        return email;
+    }
+
+    // returns customer phone number
+    public String getPhoneNumber() {
+        return phone_number;
+    }
+
+    // returns customer total order count
+    public int getOrderCount() {
+        return total_orders;
+    }
+
+    // returns customer last order date
+    public String getLastOrderDate() {
+        return last_order_date;
+    }
+
+    // generates a new customer ID
+    public int getNewID() {
+        return customer_id++;
+    }
+
+    // updates customer total order count
+    public int updateOrderCount() {
+        return total_orders++;
+    }
+
+    // updates customer last order date
+    public String updateLastOrderDate() {
+        return last_order_date;
+    }
+
+    // adds customer to customer list
+    public void AddCustomerToList(ArrayList<Customer> Customers) {
+        this.Customers = Customers;
+    }
 }
--- Supplier.java
+++ Supplier.java
@@ -1,12 +1,105 @@
+import java.util.ArrayList;
+
 public class Supplier {
     public String name;
-    public int supplier_id;
+    public int supplier_id = 5;
     public String description;
     public String email;
     public String phone_number;
     public String postcode;
     public String address;
-    public int total_orders;
-    public String last_order_date;
+    public int total_orders = 0;
+    public String last_order_date = "None";
     public static Material[] material;
+    public ArrayList<Supplier> Suppliers;
+
+    // constructor
+    public Supplier(
+            String name,
+            String description,
+            String email,
+            String phone_number,
+            String postcode,
+            String address,
+            Material[] material
+    ) {
+        this.name = name;
+        this.supplier_id = getNewID();
+        this.description = description;
+        this.email = email;
+        this.phone_number = phone_number;
+        this.postcode = postcode;
+        this.address = address;
+        Supplier.material = material;
+    }
+
+    // returns supplier name
+    public String getName() {
+        return name;
+    }
+
+    // returns supplier id
+    public int getID() {
+        return supplier_id;
+    }
+
+    // returns supplier description
+    public String getDescription() {
+        return description;
+    }
+
+    // returns supplier email
+    public String getEmail() {
+        return email;
+    }
+
+    // returns supplier phone number
+    public String getPhoneNumber() {
+        return phone_number;
+    }
+
+    // returns supplier postcode
+    public String getPostcode() {
+        return postcode;
+    }
+
+    // returns supplier address
+    public String getAddress() {
+        return address;
+    }
+
+    // returns supplier total order count
+    public int getOrderCount() {
+        return total_orders;
+    }
+
+    // returns supplier last order date
+    public String getLastOrderDate() {
+        return last_order_date;
+    }
+
+    // returns supplier material
+    public Material[] getMaterial() {
+        return material;
+    }
+
+    // generates a new supplier ID
+    public int getNewID() {
+        return supplier_id++;
+    }
+
+    // updates supplier total order count
+    public int updateOrderCount() {
+        return total_orders++;
+    }
+
+    // updates supplier last order date
+    public String updateLastOrderDate() {
+        return last_order_date;
+    }
+
+    // adds supplier to supplier list
+    public void AddSupplierToList(ArrayList<Supplier> Suppliers) {
+        this.Suppliers = Suppliers;
+    }
 }
