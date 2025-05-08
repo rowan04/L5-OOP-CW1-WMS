08fd51dcd3c85ffc81e17457a9d64477bdefc3e3
rowan04 <rowan.moss@stfc.ac.uk>
2025-05-08 16:43:17 +0100

    Add set methods for supplier class

--- Supplier.java
+++ Supplier.java
@@ -1,35 +1,31 @@
 import java.util.ArrayList;
 
 public class Supplier {
-    public String name;
-    public int supplier_id = 5;
-    public String description;
-    public String email;
-    public String phone_number;
-    public String postcode;
-    public String address;
-    public int total_orders = 0;
-    public String last_order_date = "None";
-    public static Material[] material;
-    public ArrayList<Supplier> Suppliers;
+    private String name;
+    private int supplier_id = 5;
+    private String email;
+    private String phone_number;
+    private String address;
+    private int total_orders;
+    private String last_order_date;
+    private static Material[] material;
+    private ArrayList<Supplier> Suppliers;
 
     // constructor
     public Supplier(
             String name,
-            String description,
             String email,
             String phone_number,
-            String postcode,
             String address,
             Material[] material
     ) {
         this.name = name;
-        this.supplier_id = getNewID();
-        this.description = description;
+        this.supplier_id = setID(supplier_id);
         this.email = email;
         this.phone_number = phone_number;
-        this.postcode = postcode;
         this.address = address;
+        this.total_orders = 0;
+        this.last_order_date = "none";
         Supplier.material = material;
     }
 
@@ -43,11 +39,6 @@ public class Supplier {
         return supplier_id;
     }
 
-    // returns supplier description
-    public String getDescription() {
-        return description;
-    }
-
     // returns supplier email
     public String getEmail() {
         return email;
@@ -56,11 +47,6 @@ public class Supplier {
     // returns supplier phone number
     public String getPhoneNumber() {
         return phone_number;
-    }
-
-    // returns supplier postcode
-    public String getPostcode() {
-        return postcode;
     }
 
     // returns supplier address
@@ -83,19 +69,40 @@ public class Supplier {
         return material;
     }
 
+    // sets a new supplier name
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
     // generates a new supplier ID
-    public int getNewID() {
-        return supplier_id++;
+    public int setID(int supplierId) {
+        this.supplier_id = supplierId++;
+        return supplier_id;
+    }
+
+    // sets a new supplier email
+    public void setEmail(String newEmail) {
+        this.email = newEmail;
+    }
+
+    // sets a new supplier phone number
+    public void setPhoneNumber(String newPhoneNumber) {
+        this.phone_number = newPhoneNumber;
+    }
+
+    // sets a new supplier address
+    public void setAddress(String newAddress) {
+        this.address = newAddress;
+    }
+
+    // sets a new supplier last order date
+    public void setLastOrderDate(String newLastOrderDate) {
+        this.last_order_date = newLastOrderDate;
     }
 
     // updates supplier total order count
-    public int updateOrderCount() {
-        return total_orders++;
-    }
-
-    // updates supplier last order date
-    public String updateLastOrderDate() {
-        return last_order_date;
+    public void updateOrderCount() {
+        total_orders++;
     }
 
     // adds supplier to supplier list
