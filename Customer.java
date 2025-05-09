287c1d9e597c32ba13bdd28d011dc7c3f8e282e6
rowan04 <rowan.moss@stfc.ac.uk>
2025-05-09 15:51:46 +0100

    Add set methods to customer class and add default customers to list

--- Customer.java
+++ Customer.java
@@ -1,30 +1,39 @@
 import java.util.ArrayList;
 
 public class Customer {
-    public int customer_id = 3;
-    public String name;
-    public String email;
-    public String phone_number;
-    public int total_orders = 0;
-    public String last_order_date = "None";
-    public ArrayList<Customer> Customers;
+    private String name;
+    private int customer_id = 3;
+    private String email;
+    private String phone_number;
+    private String address;
+    private int total_orders;
+    private String last_order_date;
+    private ArrayList<Customer> customerList;
 
     // constructor
     public Customer(
-            int customer_id,
             String name,
             String email,
-            String phone_number
+            String phone_number,
+            String address
     ) {
-        this.customer_id = getNewID();
         this.name = name;
+        this.customer_id = setID();
         this.email = email;
         this.phone_number = phone_number;
+        this.address = address;
+        this.total_orders = 0;
+        this.last_order_date = "None";
     }
 
     // returns customer name
     public String getName() {
         return name;
+    }
+
+    // returns customer id
+    public int getID() {
+        return customer_id;
     }
 
     // returns customer email
@@ -37,6 +46,10 @@ public class Customer {
         return phone_number;
     }
 
+    public String getAddress() {
+        return address;
+    }
+
     // returns customer total order count
     public int getOrderCount() {
         return total_orders;
@@ -47,9 +60,37 @@ public class Customer {
         return last_order_date;
     }
 
-    // generates a new customer ID
-    public int getNewID() {
-        return customer_id++;
+    // sets a new customer name
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    // sets a new customer ID
+    public int setID() {
+        // find the length of customer list, or set to 0 if it doesn't exist
+        int current = customerList != null ? customerList.size() : 0;
+        this.customer_id = ++current;
+        return this.customer_id;
+    }
+
+    // sets a new customer email
+    public void setEmail(String newEmail) {
+        this.email = newEmail;
+    }
+
+    // sets a new customer phone number
+    public void setPhoneNumber(String newPhoneNumber) {
+        this.phone_number = newPhoneNumber;
+    }
+
+    // sets a new customer address
+    public void setAddress(String newAddress) {
+        this.address = newAddress;
+    }
+
+    // sets a new customer last order date
+    public void setLastOrderDate(String newLastOrderDate) {
+        this.last_order_date = newLastOrderDate;
     }
 
     // updates customer total order count
@@ -63,7 +104,59 @@ public class Customer {
     }
 
     // adds customer to customer list
-    public void AddCustomerToList(ArrayList<Customer> Customers) {
-        this.Customers = Customers;
+    public void AddCustomerToList(ArrayList<Customer> customerList) {
+        this.customerList = customerList;
+    }
+}
+
+class createDefCustomers {
+    public static void main(String[] args) {
+        ArrayList<Customer> customerList = new ArrayList<Customer>();
+
+        addDefaultCustomers(customerList);
+
+        System.out.println("Default customers added");
+    }
+
+    public static void addDefaultCustomers(ArrayList<Customer> customerList) {
+        Customer customer1 = new Customer(
+                "Josh Davies",
+                "joshdg@gmail.com",
+                "07849 749739",
+                "44 Cricket Road, Ascott, Oxfordshire, OX7 6AY"
+        );
+        customerList.add(customer1);
+
+        Customer customer2 = new Customer(
+                "Rollo Flagman",
+                "flagman@supervillain.org",
+                "07999 666424",
+                "14 High Street, Gotham, DC, GT4 2EZ"
+        );
+        customerList.add(customer2);
+
+        Customer customer3 = new Customer(
+                "Jamie Knight",
+                "jkknight@gmail.com",
+                "07181 764289",
+                "5 Falcon Villas, Hundley "
+        );
+        customerList.add(customer3);
+
+        Customer customer4 = new Customer(
+                "Cerys Morgan",
+                "cm27oop@outlook.co.uk",
+                "07797 124569",
+                "12 Waterside Close, Rogerstone, Newport, NP10 0BB"
+        );
+        customerList.add(customer4);
+
+        Customer customer5 = new Customer(
+                "Padme McLaren",
+                "pmclaren@gmail.com",
+                "079111 422013",
+                "3 Coruscant Central, Newbury, RG1 1GR"
+        );
+        customerList.add(customer5);
     }
 }
