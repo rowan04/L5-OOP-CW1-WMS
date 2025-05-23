97554a822b4bde0445ae82162eb4606c3bdc8e1d
rowan04 <rowan.moss@stfc.ac.uk>
2025-05-24 00:53:22 +0100

    Changes to switch procedure and main file to allow program to continuously run

--- Main.java
+++ Main.java
@@ -22,40 +22,66 @@ public class Main {
         // Start scanner
         Scanner scan = new Scanner(System.in);
 
-        // output options for user
-        System.out.println("Enter 1 to view current stock levels.");
-        System.out.println("Enter 2 to order new stock.");
-        System.out.println("Enter 3 to enter a customer order.");
-        System.out.println("Enter 4 to view supplier information.");
-        System.out.println("Enter 5 to update supplier records.");
-        System.out.println("Enter 6 to view finances.");
-        System.out.println("Enter 7 to create a financial report.");
+        // make a boolean variable that keeps the program running
+        boolean run = true;
 
-        // uses switch statement to provide different options for user's input
-        char entry = scan.next().charAt(0);
-        switch(entry) {
-            case '1':
-                for (Item item : stockList) {
-                    InventoryManager.displayStockCount(item);
-                    item.checkStockLevel();
-                }
+        while(run) {
+            // output options for user
+            System.out.println("Enter 1 to view current stock levels.");
+            System.out.println("Enter 2 to order new stock.");
+            System.out.println("Enter 3 to enter a customer order.");
+            System.out.println("Enter 4 to view supplier information.");
+            System.out.println("Enter 5 to update supplier records.");
+            System.out.println("Enter 6 to view finances.");
+            System.out.println("Enter 7 to create a financial report.");
+            System.out.println("Enter 8 to exit.");
 
-            case '2':
+            // uses switch statement to provide different options for user's input
+            char entry = scan.next().charAt(0);
 
-            case '3':
+            switch (entry) {
+                case '1':
+                    for (Item item : stockList) {
+                        InventoryManager.displayStockCount(item);
+                        item.checkStockLevel();
+                    }
+                    // Delay to give user time to read output
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    break;
 
-            case '4':
+                case '2':
 
-            case '5':
+                case '3':
 
-            case '6':
+                case '4':
 
-            case '7':
+                case '5':
 
-            default:
-                // called when user does an invalid input
-                System.out.println("Invalid entry.");
-        };
+                case '6':
+
+                case '7':
+
+                case '8':
+                    System.out.println("Goodbye!");
+                    run = false;
+                    break;
+
+                default:
+                    // called when user does an invalid input
+                    System.out.println("Invalid entry.");
+                    // Delay to give user time to read output
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    break;
+            };
+        }
 
         // close scanner
         scan.close();
